2c415b781a3e4012b93fdd1ad2ee94321509c116
Rasalahmad <rasalahmadsit@gmail.com>
2021-09-01 12:16:23 +0600

    error handle

--- js/weather.js
+++ js/weather.js
@@ -2,23 +2,37 @@ const loadWeather = () => {
     const searchResult = document.getElementById('search-input');
     const searchText = searchResult.value;
     searchResult.value = '';
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${searchText}&appid=c7c61809061789d239a583029c7302aa`
-    fetch(url)
-        .then(res => res.json())
-        .then(data => displayWeather(data))
+    const showWeather = document.getElementById('show-weather');
+    if (searchText.length == 0) {
+        showWeather.innerHTML = `
+        <h1 class = 'error'>Enter your city</h1>
+        `;
+    } else {
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${searchText}&appid=c7c61809061789d239a583029c7302aa`
+        fetch(url)
+            .then(res => res.json())
+            .then(data => displayWeather(data))
+    }
 };
 
 const displayWeather = (details) => {
-    // console.log(details)
+    console.log(details)
     const showWeather = document.getElementById('show-weather');
-    const div = document.createElement('div');
-    showWeather.textContent = '';
-    div.classList.add('main')
-    div.innerHTML = `
-    <img src="http://openweathermap.org/img/w/${details.weather[0].icon}.png">
+    if (details.message === 'city not found') {
+        showWeather.innerHTML = `
+        <h1 class = 'error'>City not found</h1>
+        `
+    } else {
+
+        const div = document.createElement('div');
+        showWeather.textContent = '';
+        div.classList.add('main')
+        div.innerHTML = `
+    <img class ='img' src="http://openweathermap.org/img/w/${details.weather[0].icon}.png">
     <h1>${details.name}</h1>
     <h1>${((details.main.temp)-273.15).toFixed(1)}&deg;C</h1>
     <h1>${details.weather[0].description}</h1>
     `
-    showWeather.appendChild(div)
+        showWeather.appendChild(div)
+    }
 }
