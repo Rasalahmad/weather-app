298e09b627c178ba00c0b358e3e244f13441c9a8
Rasalahmad <rasalahmadsit@gmail.com>
2021-09-01 17:54:19 +0600

    update

--- js/weather.js
+++ js/weather.js
@@ -1,14 +1,16 @@
 const loadWeather = () => {
     const searchResult = document.getElementById('search-input');
+    const spinner = document.getElementById('spinner');
     const searchText = searchResult.value;
     searchResult.value = '';
     const showWeather = document.getElementById('show-weather');
-    if (searchText.length == 0) {
+    if (searchText == '') {
         showWeather.innerHTML = `
         <h1 class = 'error'>Enter your city</h1>
         `;
     } else {
         const url = `https://api.openweathermap.org/data/2.5/weather?q=${searchText}&appid=c7c61809061789d239a583029c7302aa`
+        spinner.classList.remove('d-none');
         fetch(url)
             .then(res => res.json())
             .then(data => displayWeather(data))
@@ -20,13 +22,15 @@ const displayWeather = (details) => {
     const showWeather = document.getElementById('show-weather');
     if (details.message === 'city not found') {
         showWeather.innerHTML = `
-        <h1 class = 'error'>City not found</h1>
+        <h1 class = 'error'>your city is not found</h1>
         `
+        spinner.classList.add('d-none');
     } else {
 
         const div = document.createElement('div');
         showWeather.textContent = '';
         div.classList.add('main')
+        spinner.classList.add('d-none');
         div.innerHTML = `
     <img class ='img' src="http://openweathermap.org/img/w/${details.weather[0].icon}.png">
     <h1>${details.name}</h1>
